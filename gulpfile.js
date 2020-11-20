78b3f7044ed20d72c9e14333eda37f584427108c
Michał <mirossian@gmail.com>
2020-11-20 15:17:41 +0100

    add calculator to HTML and Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,11 @@
 const gulp = require("gulp");
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
-const notifier = require('node-notifier');
-const entryPath = ".";
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+const notifier = require("node-notifier");
 
-
-
-sass.compiler = require('sass');
-
-function showError(err) {
-    console.log("------------------");
-    console.log(err.messageFormatted);
-    console.log("------------------");
-
-    notifier.notify({
-        title: 'Błąd',
-        message: err.messageFormatted
-    });
-}
-
+sass.compiler = require("sass");
 
 function server(cb) {
     browserSync.init({
@@ -28,29 +13,40 @@ function server(cb) {
             baseDir: "./"
         }
     });
+
     cb();
 }
 
-function makeCSS() {
-    return gulp.src('./scss/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: "expanded" //d:nested, compact, compressed, expanded
-        }).on('error', showError))
+function notifyError(err) {
+    console.log(err.messageFormatted);
+    notifier.notify({
+        title: "Błąd",
+        message: err.formatted
+    });
+}
+
+function makeCss() {
+    return gulp
+        .src("./scss/main.scss")
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sass({ outputStyle: "expanded" }).on("error", notifyError))
         .pipe(autoprefixer())
         .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 }
 
+// gulp.series(makeCss)
+// gulp.parallel(makeCss)
+
 function watch(cb) {
-    gulp.watch("./scss/**/*.scss", gulp.series(makeCSS));
-    gulp.watch("/*.html").on('change', browserSync.reload);
+    gulp.watch("./scss/**/*.scss", makeCss);
+    gulp.watch("./**/*.js").on("change", browserSync.reload);
+    gulp.watch("./*.html").on("change", browserSync.reload);
+
     cb();
 }
 
-module.exports.makeCSS = makeCSS;
+module.exports.makeCss = makeCss;
 module.exports.watch = watch;
-module.exports.default = gulp.series(server, makeCSS, watch)
-// powoduje że wykonuje wszystkie zadania
-
+module.exports.default = gulp.series(server, makeCss, watch);
